7141d68456f579c5182a650ef983dd7ba067495b
taaacse4 <selim.acerbas@swisscom.com>
2025-11-01 20:30:46 +0100

    seperate routers and app

--- fastapi/todo_app/db.py
+++ fastapi/todo_app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
 # It allows us to kind of a connnection to the database. We want to check multiple thread in case there is something happening there as well. Because sql is good at doing that.
 engine = create_engine(
